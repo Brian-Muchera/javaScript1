16ca9ce094b4abb6143eda4412bad197cdce09a1
Brian-Muchera <mucherabrian2@gmail.com>
2020-07-20 11:16:15 +0300

    link corrected

--- css/js/scripts.js
+++ css/js/scripts.js
@@ -1,33 +1,27 @@
-function getAkanName() {
-    var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    var maleAkanNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
-    var femaleAkanNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
+function getAkanName(){
+    var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    var maleAkanNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"]
+    var femaleAkanNames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"]
     var myBirthday = document.getElementById("myBirthDate").value;
-    var myGender = document.getElementByName("gender");
+    var myGender = document.getElementsByName("gender");
     var dateOfBirth = new Date(myBirthday);
-    var daysOfTheWeek = dateOfBirth.getDay();
-
-    if (myBirthday === "") {
-        document.getElementById('message').innerHTML = "Be serious!Invalid character!!";
+    var dayOfTheWeek = dateOfBirth.getDay();
+    if(myBirthday === ""){
+        document.getElementById('message').innerHTML = "<div> You Didn't Submit a Valid Date!</div>";
     }
     else {
-        for (var i = 0; i < myGender.length; i++) {
-            if (myGender[i].checked) {
-                if (myGender[i].value === "Male") {
-                    alert("Born on a " + daysOfTheWeek[dateOfBirth.getDay()] + " " + "Your Akan Name is: " + maleAkanNames[dayOfTheWeek]);
-                    document.getElementById('message').innerHTML = names[dateOfBirth.getDay()];
+        for(var i=0;i<myGender.length;i++){
+            if(myGender[i].checked){
+                if(myGender[i].value === "Male"){
+                    document.getElementById('message').innerHTML = " Born on a <span>" + days[dayOfTheWeek] + "</span>, Your Akan Name's <span>" + maleAkanNames[dayOfTheWeek] + "</span>";
                 }
-                else if (myGender[i].value === "Female") {
-                    alert(" Born on a " + daysOfTheWeek[dateOfBirth.getDay()] + " " + "Your Akan Name is: " + femaleAkanNames[dayOfTheWeek]);
-                    document.getElementById('message').innerHTML = names[dateOfBirth.getDay()];
+                else {
+                    document.getElementById('message').innerHTML = " Born on a <span>" + days[dayOfTheWeek] + "</span>, Your Akan Name's <span>" + femaleAkanNames[dayOfTheWeek] + "</span>";
                 }
+                break;
             }
-            else if ('null') {
-                alert("Please select a Gender");
-                document.getElementById('message').innerHTML = names[dateOfBirth.getDay()];
-            }
-            else ("null"); {
-                alert("click Submit");
+            else {
+                document.getElementById('message').innerHTML = "<div>Please select a Gender</div>";
             }
         }
     }
