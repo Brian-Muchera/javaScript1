9f9555856e57f954f84b7ab95f21e47400513185
Brian-Muchera <mucherabrian2@gmail.com>
2020-07-20 10:05:25 +0300

    modify scripts.js

--- css/js/scripts.js
+++ css/js/scripts.js
@@ -3,14 +3,9 @@ function getAkanName() {
     var maleAkanNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
     var femaleAkanNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
     var myBirthday = document.getElementById("myBirthDate").value;
-    var myGender = document.getElementsByName("gender");
-    var dateOfBirth = new Date(dateOfBirth.getDay()
-   
-    var a = Math.floor((14 - month) / 12)
-
-    var y = year - a
-    var m = month + 12 * a - 2
-    var d = (day + y + Math.floor(y / 4) - Math.floor(y / 100) + Math.floor(year / 400) + Math.floor((31 * m / 12)) % 7)
+    var myGender = document.getElementByName("gender");
+    var dateOfBirth = new Date(myBirthday);
+    var daysOfTheWeek = dateOfBirth.getDay();
 
     if (myBirthday === "") {
         document.getElementById('message').innerHTML = "Be serious!Invalid character!!";
@@ -19,7 +14,7 @@ function getAkanName() {
         for (var i = 0; i < myGender.length; i++) {
             if (myGender[i].checked) {
                 if (myGender[i].value === "Male") {
-                    alert(" Born on a " + daysOfTheWeek[dateOfBirth.getDay()] + " " + "Your Akan Name is: " + maleAkanNames[dayOfTheWeek]);
+                    alert("Born on a " + daysOfTheWeek[dateOfBirth.getDay()] + " " + "Your Akan Name is: " + maleAkanNames[dayOfTheWeek]);
                     document.getElementById('message').innerHTML = names[dateOfBirth.getDay()];
                 }
                 else if (myGender[i].value === "Female") {
@@ -27,11 +22,12 @@ function getAkanName() {
                     document.getElementById('message').innerHTML = names[dateOfBirth.getDay()];
                 }
             }
-            else if {
-                document.getElementById('message').innerHTML = "Please select a Gender";
+            else if ('null') {
+                alert("Please select a Gender");
+                document.getElementById('message').innerHTML = names[dateOfBirth.getDay()];
             }
-            else {
-                alert("click submit")
+            else ("null"); {
+                alert("click Submit");
             }
         }
     }
